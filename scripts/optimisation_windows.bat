2397d0fda60e80e02c3764cf1a37d189c9ec3a7e
jeroval <jerome.valens@gmail.com>
2024-12-16 22:24:03 +0100

    Update optimisation_windows.bat

--- scripts/optimisation_windows.bat
+++ scripts/optimisation_windows.bat
@@ -2,10 +2,17 @@
 setlocal
 
 
+echo _____________________________________________________________________________________________________________
 echo Definir la politique d'execution de PowerShell pour permettre l'execution de scripts
+echo _____________________________________________________________________________________________________________
+
 powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope Process -Force"
 
+
+echo _____________________________________________________________________________________________________________
 echo Suppression de fichiers dans différents répertoires
+echo _____________________________________________________________________________________________________________
+
 del /F /Q "%USERPROFILE%\Downloads\*.*" > nul 2>&1
 del /S /F /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\INetCache\*.*" > nul 2>&1
 del /S /F /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\INetCookies\*.*" > nul 2>&1
@@ -18,19 +25,28 @@ del /S /F /Q "%SystemRoot%\Prefetch\*.*" > nul 2>&1
 del /F /Q "%windir%\ServiceProfiles\LocalService\AppData\Local\FontCache\*.*" > nul 2>&1
 
 
+echo _____________________________________________________________________________________________________________
 echo Vider le cache DNS
+echo _____________________________________________________________________________________________________________
+
 ipconfig /flushdns
 ipconfig /registerdns
 ipconfig /release
 ipconfig /renew
 
 
+echo _____________________________________________________________________________________________________________
 echo Nettoyage des journaux systeme
+echo _____________________________________________________________________________________________________________
+
 wevtutil cl Application
 wevtutil cl System
 
 
+echo _____________________________________________________________________________________________________________
 echo Arret des services facultatifs
+echo _____________________________________________________________________________________________________________
+
 net stop "DiagTrack"
 net stop "WSearch"
 net stop "dmwappushservice"
@@ -39,7 +55,10 @@ net stop "RemoteRegistry"
 net stop "Fax"
 
 
+echo _____________________________________________________________________________________________________________
 echo Desactivation des services facultatifs
+echo _____________________________________________________________________________________________________________
+
 sc config "DiagTrack" start= disabled
 sc config "SCardSvr" start= disabled
 sc config "WSearch" start= disabled
@@ -51,7 +70,10 @@ sc config "RemoteRegistry" start= disabled
 sc config WMPNetworkSvc start= disabled
 
 
+echo _____________________________________________________________________________________________________________
 echo Configuration réseau
+echo _____________________________________________________________________________________________________________
+
 netsh winsock reset
 netsh int tcp set global autotuninglevel=disabled
 netsh int tcp set global dca=enabled
@@ -60,25 +82,48 @@ netsh int tcp set global rss=enabled
 netsh int tcp set global chimney=disabled
 netsh int tcp set supplemental template=internet congestionprovider=none
 
+
+echo _____________________________________________________________________________________________________________
 echo Définir la MTU à 1500 octets
-netsh interface ipv4 set subinterface "Connexion au réseau local" mtu=1500 store=persistent
+echo _____________________________________________________________________________________________________________
+
+for /f "tokens=1-4*" %%A in ('netsh interface show interface ^| findstr /I "Wi-Fi"') do (
+    echo Modification de l'interface Wi-Fi : %%E
+    netsh interface ipv4 set subinterface "%%E" mtu=1400 store=persistent
+    if %errorlevel%==0 (
+        echo MTU configuré avec succès pour "%%E".
+    ) else (
+        echo Erreur lors de la configuration pour "%%E".
+    )
+)
 
 
+echo _____________________________________________________________________________________________________________
 echo Désactiver IPV6
+echo _____________________________________________________________________________________________________________
+
 netsh interface ipv6 set privacy disabled
 netsh interface teredo set state disabled
 
 
+echo _____________________________________________________________________________________________________________
 echo Configurer le pare-feu Windows
+echo _____________________________________________________________________________________________________________
+
 netsh advfirewall set allprofiles state on
 
 
-
+echo _____________________________________________________________________________________________________________
 echo Suppression du dossier Windows.old
+echo _____________________________________________________________________________________________________________
+
 rd /s /q "%Windir%\Windows.old"
 
 
+echo _____________________________________________________________________________________________________________
 echo Modification du registre pour diverses optimisations et désactivations
+echo _____________________________________________________________________________________________________________
+
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v UserPreferencesMask /t REG_BINARY /d 9012038010000000 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop\WindowMetrics" /v MinAnimate /t REG_SZ /d 0 /f
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\InputPersonalization" /v RestrictImplicitTextCollection /t REG_DWORD /d 1 /f
@@ -119,27 +164,41 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v AllowAdministratorLockout
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v NoLMHash /t REG_DWORD /d 1 /f
 
 
+echo _____________________________________________________________________________________________________________
 echo Vérification des fichiers système
+echo _____________________________________________________________________________________________________________
+
 sfc /scannow
 
 
+echo _____________________________________________________________________________________________________________
 echo Mettre à jour les définitions de Windows Defender
+echo _____________________________________________________________________________________________________________
+
 "%ProgramFiles%\Windows Defender\MpCmdRun.exe" -SignatureUpdate
 
 
+echo _____________________________________________________________________________________________________________
 echo Effectuer une analyse complète avec Windows Defender
+echo _____________________________________________________________________________________________________________
+
 "%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType 2
 
 
+echo _____________________________________________________________________________________________________________
 echo definir les politiques de securite sur les comptes
+echo _____________________________________________________________________________________________________________
+
 net accounts /lockoutduration:15
 net accounts /lockoutthreshold:5
 net accounts /lockoutwindow:15
 net user Guest /active:no
 
 
-
+echo _____________________________________________________________________________________________________________
 echo Suppression de certains fichiers dans le répertoire Explorer
+echo _____________________________________________________________________________________________________________
+
 start "" /b "cmd /c echo y | del /F /Q %USERPROFILE%\AppData\Local\Microsoft\Windows\Explorer\*.bak"
 start "" /b "cmd /c echo y | del /F /Q %USERPROFILE%\AppData\Local\Microsoft\Windows\Explorer\*.ini"
 start "" /b "cmd /c echo y | del /F /Q %USERPROFILE%\AppData\Local\Microsoft\Windows\Explorer\*.log"
@@ -148,13 +207,18 @@ start "" /b "cmd /c echo y | del /F /Q %USERPROFILE%\AppData\Local\Microsoft\Win
 start "" /b "cmd /c echo y | del /F /Q %USERPROFILE%\AppData\Local\Microsoft\Windows\INetCookies\*.*"
 
 
+echo _____________________________________________________________________________________________________________
 echo Desactivation des fonctionnalites inutiles de Windows
+echo _____________________________________________________________________________________________________________
 
 dism /online /disable-feature /featurename:Internet-Explorer-Optional-amd64 /NoRestart
 dism /online /disable-feature /featurename:WindowsMediaPlayer /NoRestart
 
 
+echo _____________________________________________________________________________________________________________
 echo Terminer certains processus
+echo _____________________________________________________________________________________________________________
+
 taskkill /F /IM ASCTray.exe
 taskkill /F /IM CalculatorApp.exe
 taskkill /F /IM Code.exe
@@ -196,8 +260,10 @@ taskkill /F /IM Xbox*
 taskkill /F /IM gamingservices.exe
 
 
-
+echo _____________________________________________________________________________________________________________
 echo Ajouter la disposition de clavier QWERTY des États-Unis 
+echo _____________________________________________________________________________________________________________
+
 Install-Language -Language en-US
 Set-WinUILanguageOverride -Language en-US
 Get-WinUserLanguageList
@@ -205,26 +271,40 @@ $LangList = New-WinUserLanguageList en-US
 Set-WinUserLanguageList $LangList
 
 
-
+echo _____________________________________________________________________________________________________________
 echo Recherche de mises à jour Windows Update
+echo _____________________________________________________________________________________________________________
+
 wuauclt /detectnow /updatenow
 wuauclt.exe /updatenow
 
+
+echo _____________________________________________________________________________________________________________
 echo Mettre à jour les pilotes réseau
+echo _____________________________________________________________________________________________________________
+
 pnputil /scan-devices
 
 
+echo _____________________________________________________________________________________________________________
 echo Installer et import du module PSWindowsUpdate
+echo _____________________________________________________________________________________________________________
+
 powershell -Command "Install-Module -Name PSWindowsUpdate -Force -SkipPublisherCheck"
 powershell -Command "Import-Module PSWindowsUpdate"
 
 
+echo _____________________________________________________________________________________________________________
 echo Recherche et installation des mises à jour Windows
+echo _____________________________________________________________________________________________________________
+
 powershell -Command "Get-WindowsUpdate -AcceptAll -IgnoreReboot"
 powershell -Command "Install-WindowsUpdate -AcceptAll -IgnoreReboot"
 
-
+echo _____________________________________________________________________________________________________________
 echo Créer un fichier PowerShell temporaire pour mise à jour windows
+echo _____________________________________________________________________________________________________________
+
 set TempScript=%TEMP%\temp_update_script.ps1
 
 echo function Force-WindowsUpdates { > %TempScript%
@@ -252,14 +332,24 @@ echo } >> %TempScript%
 echo Force-WindowsUpdates >> %TempScript%
 
 
+echo _____________________________________________________________________________________________________________
 echo Exécuter le script PowerShell temporaire
+echo _____________________________________________________________________________________________________________
+
 powershell -ExecutionPolicy Bypass -File %TempScript%
 
 
+echo _____________________________________________________________________________________________________________
 echo Supprimer le script PowerShell temporaire
+echo _____________________________________________________________________________________________________________
+
 del %TempScript%
 
 
+echo _____________________________________________________________________________________________________________
 echo Nettoyage et optimisation terminés !
+echo _____________________________________________________________________________________________________________
+
+
 pause
 endlocal
